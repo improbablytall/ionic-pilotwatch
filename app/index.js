90a80468d7069d437d8469928f58c9e21e6c9a05
improbablytall <chrisduncalf@gmail.com>
2017-10-12 09:36:19 +0000

    updated from studio!

--- app/index.js
+++ app/index.js
@@ -2,22 +2,29 @@ import clock from "clock";
 import document from "document";
 
 import * as util from "../common/utils";
+import * as moment from "../common/moment";
 
 // Update the clock every minute
-clock.granularity = "minutes";
+clock.granularity = "seconds";
+var zuluOffset = 1;
 
 // Get a handle on the <text> element
-let myLabel = document.getElementById("myLabel");
-
+let mainClock = document.getElementById("mainClock");
+let zuluClock = document.getElementById("zuluClock");
+let scratchPad = document.getElementById("scratchPad");
 // Update the <text> element with the current time
 function updateClock() {
   let today = new Date();
   let hours = today.getHours();
   let mins = util.zeroPad(today.getMinutes());
+  let secs = util.zeroPad(today.getSeconds());
+  
+  let zuluHours = hours-zuluOffset
 
-  myLabel.innerText = `${hours}:${mins}`;
+  mainClock.innerText = `${hours}:${mins}:${secs}`;
+  zuluClock.innerText = `${zuluHours}:${mins}:${secs}`;
+  
 }
-
 // Update the clock every tick event
 clock.ontick = () => updateClock();
 
