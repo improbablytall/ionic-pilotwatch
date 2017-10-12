cf0dce7702d65cf7908c73c4c94a694dbe17e5cd
improbablytall <chrisduncalf@gmail.com>
2017-10-12 09:56:03 +0000

    mike first pass tidy

--- app/index.js
+++ app/index.js
@@ -2,16 +2,17 @@ import clock from "clock";
 import document from "document";
 
 import * as util from "../common/utils";
-import * as moment from "../common/moment";
 
-// Update the clock every minute
+// Update the clock every second
 clock.granularity = "seconds";
-var zuluOffset = 1;
 
 // Get a handle on the <text> element
 let mainClock = document.getElementById("mainClock");
 let zuluClock = document.getElementById("zuluClock");
 let scratchPad = document.getElementById("scratchPad");
+
+var zuluOffset = 1; //offset from Zulu to Local
+
 // Update the <text> element with the current time
 function updateClock() {
   let today = new Date();
@@ -19,12 +20,13 @@ function updateClock() {
   let mins = util.zeroPad(today.getMinutes());
   let secs = util.zeroPad(today.getSeconds());
   
-  let zuluHours = hours-zuluOffset
+  let zuluHours = hours - zuluOffset;
 
   mainClock.innerText = `${hours}:${mins}:${secs}`;
-  zuluClock.innerText = `${zuluHours}:${mins}:${secs}`;
+  zuluClock.innerText = `${zuluHours}:${mins}:${secs}z`;
   
 }
+
 // Update the clock every tick event
 clock.ontick = () => updateClock();
 
--- common/utils.js
+++ common/utils.js
@@ -1,7 +1,4 @@
 // Add zero in front of numbers < 10
 export function zeroPad(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
+  return i <= 9 ? "0"+i : i;
 }
